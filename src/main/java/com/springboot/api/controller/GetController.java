fc59db78f77bb0eac5b5b87191aba946adcda46f
DevLeeYG <hi33741@naver.com>
2022-07-11 10:04:53 +0900

    'getController'

--- src/main/java/com/springboot/api/controller/GetController.java
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -1,61 +1,14 @@
 package com.springboot.api.controller;
 
-import com.springboot.api.dto.MemberDto;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/get-api")
 public class GetController {
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello";
+
+    @GetMapping
+    public String getControll(){
+        return "asdf";
     }
 
-    @GetMapping( "/variable1/{asdf}")
-    public String getVari(@PathVariable String asdf){
-        return asdf;
-    }
-
-    @GetMapping(value = "/request")
-    public String getRequest( @RequestParam String name,
-                              @RequestParam String email,@RequestParam String organization){
-
-        return name + " " + email + " " + organization;
-
-    }
-
-    @GetMapping("/request2")
-    public String getRequest2(@RequestParam Map<String,String> param){
-
-            StringBuilder sb = new StringBuilder();
-
-            param.entrySet().forEach((el)->{
-               sb.append(el.getKey() + ":" + el.getValue() +"\n");
-            });
-
-        return sb.toString();
-    }
-
-    @GetMapping("/request3")
-    public String getRequestParam3(@RequestParam Map<String,String> param){
-       StringBuilder sb = new StringBuilder();
-
-       param.entrySet().forEach((map)->{
-           sb.append(map.getKey() +" :" + map.getValue() + "\n");
-       });
-
-        return sb.toString();
-
-        };
-
-    @GetMapping("/request4")
-    public String getRequestParam3(MemberDto memberDto){
-        return memberDto.getName() + " " + memberDto.getEmail() +" " + memberDto.getOrganization();
-    }
-
-    }
-
-
-
+}
